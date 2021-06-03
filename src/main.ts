6c6281fcac63d37d08fb9810069964a651c61b10
argon1025 <argon1025@gmail.com>
2021-06-03 21:38:08 +0900

    Add 데코레이터 적용연습

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+declare const module: any;
+
 async function bootstrap() {
   const port = process.env.port || 3000;
   const app = await NestFactory.create(AppModule);
   await app.listen(port);
+  console.log(`[INFO] Started server! -> http://localhost:${port}`)
+
+  if (module.hot) {
+    console.log("[INFO] HotLoad Activate")
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
