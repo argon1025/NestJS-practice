2cba511f723828d53ad524fe27ea1bafed1178fe
argon1025 <argon1025@gmail.com>
2021-06-04 13:41:14 +0900

    Add HotLoad 추가

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+// 리퀘스트 데이터와 리스폰스 데이터를 받아서 비지니스 로직을 호출하는형태
 @Controller("v1")
 export class AppController {
   constructor(private readonly appService: AppService) {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 
+// 트랜젝션 단위로 기능을 분리
 @Injectable()
 export class AppService {
   getHello(): string {
